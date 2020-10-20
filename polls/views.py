27b1d657fe5822300bdc56532661eccffb8949b0
nuttapol-kor <nuttapol.kor@ku.th>
2020-10-20 22:13:29 +0700

    Only authenticated user can submit vote

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import Choice, Question
 
@@ -38,7 +39,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-
+@login_required
 def vote(request, question_id):
     """Vote a choice in the question."""
     question = get_object_or_404(Question, pk=question_id)
